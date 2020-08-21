70742931713168387d918f1e272517a1fac69dd1
Jake <git@ja-ke.tech>
2020-08-21 10:44:11 +0200

    Bump version to 1.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ble-serial",
-    version="1.2.0",
+    version="1.2.1",
     author="Jake",
     author_email="ble-serial-pypi@ja-ke.tech",
     description="A package to connect BLE serial adapters",
