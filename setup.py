a4b49682799778768bc729b069c6d1e69ffa880a
Jake <git@ja-ke.tech>
2021-03-29 16:20:37 +0200

    Change py requires to 3.7 because of asyncio

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
         'bleak >= 0.11.0'
     ],
