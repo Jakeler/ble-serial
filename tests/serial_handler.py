726b26c7f2df119b625feb4e2bd4235aee9e24ff
Jake <git@ja-ke.tech>
2021-04-16 17:50:44 +0200

    Test run function with more info #22

--- tests/serial_handler.py
+++ tests/serial_handler.py
@@ -2,7 +2,7 @@ from serial import Serial
 from time import sleep, perf_counter
 import subprocess, os, signal
 
-def read_serial(port: str, conn_baud: int, expected_data: bytes, end_func):
+def read_serial(port: str, conn_baud: int, expected_data: bytes):
     buffer = bytearray()
     timeout = 1.0
     with Serial(port, conn_baud, timeout=timeout) as ser:
@@ -19,11 +19,11 @@ def read_serial(port: str, conn_baud: int, expected_data: bytes, end_func):
     print(f'Completed read {len(buffer)} bytes in {tt:.3f} s')
     print(f'Rate {rate:.2f} byte/s = {rate*8:.0f} bit/s = {rate*10:.0f} baud')
 
-    end_func() # stop ble-serial here
-
     return {
         'valid': expected_data == buffer,
-        'loss_percent': (1 - len(buffer) / len(expected_data)) * 100
+        'loss_percent': (1 - len(buffer) / len(expected_data)) * 100,
+        'rx_bits': int(rate*8),
+        'rx_baud': int(rate*10),
     }
 
 def write_serial(port: str, conn_baud: int, data: bytes, chunk_size: int, delay: float):
--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor as TPE
 from time import sleep
 
 from hm11_at_config import set_module_baud
@@ -8,29 +8,74 @@ from serial_handler import read_serial, write_serial, run_ble_serial, signal_ser
 PORT_UART = '/dev/ttyUSB0'
 PORT_BLE = '/tmp/ttyBLE'
 
+PACKET_SIZE = 16
+DELAY = 0.005
+
 with open('../README.md', 'rb') as f:
     CONTENT = f.read()
     # print(CONTENT)
 
-CONTENT = CONTENT[:1000]
+CONTENT = CONTENT[:5000]
 
-# set_module_baud(PORT_UART, 19200, 9600)
+class Dir:
+    _ports = [
+        (PORT_BLE, PORT_UART),
+        (PORT_UART, PORT_BLE),
+    ]
 
-baud_to_test = [9600, 19200, 115200, 230400]
+    @classmethod
+    def BLE_UART(cls):
+        cls(0)
+
+    @classmethod
+    def UART_BLE(cls):
+        cls(1)
+
+    def __init__(self, dir: int):
+        self.id = dir
+        self.write = self._ports[dir][0]
+        self.read = self._ports[dir][1]
+
+    def __str__(self):
+        return ('BLE >> UART', 'UART >> BLE')[self.id]
+
+
+def run_test(exc: TPE, dir: Dir, baud: int, packet_size: int, delay: float):
+    futw = executor.submit(write_serial, dir.write, baud, CONTENT, PACKET_SIZE, DELAY)
+    futr = executor.submit(read_serial, dir.read, baud, CONTENT)
+    
+    result = futr.result()
+    result.update({
+        'dir': dir,
+        'rated_baud': baud,
+        'packet_size': packet_size,
+        'delay': delay,
+    })
+    print(result)
+    return result
+
+
+baud_to_test = [9600, 19200, 115200]
 prev = baud_to_test[0]
 
-for baud in baud_to_test:
-    print(f'\nTesting baud: {baud}')
-
-    set_module_baud(PORT_UART, prev, baud)
-    prev = baud
-
-    with ThreadPoolExecutor(max_workers=3) as executor:
-        futb = executor.submit(run_ble_serial)
-        sleep(3)
-        futw = executor.submit(write_serial, PORT_UART, baud, CONTENT, 8, 0.010)
-        futr = executor.submit(read_serial, PORT_BLE, baud, CONTENT, signal_serial_end)
-        print(futr.result())
+if __name__ == "__main__":
+    # Reset to start baud after fail
+    # set_module_baud(PORT_UART, 19200, 9600)
 
 
-set_module_baud(PORT_UART, prev, baud_to_test[0])
+    for baud in baud_to_test:
+        print(f'\nTesting baud: {baud}')
+
+        set_module_baud(PORT_UART, prev, baud)
+        prev = baud
+
+        with TPE(max_workers=3) as executor:
+            futb = executor.submit(run_ble_serial)
+            sleep(3)
+
+            run_test(executor, Dir.BLE_UART(), baud, PACKET_SIZE, DELAY)
+            run_test(executor, Dir.UART_BLE(), baud, PACKET_SIZE, DELAY)
+
+            signal_serial_end()
+
+    set_module_baud(PORT_UART, prev, baud_to_test[0])
