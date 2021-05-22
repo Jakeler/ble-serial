cad3cf25c593d26bece85ec8c9999def4d71a93b
Jake <git@ja-ke.tech>
2021-05-22 02:39:21 +0200

    Add another characteristic to builtin list
    works with #28

--- ble_serial/bluetooth/constants.py
+++ ble_serial/bluetooth/constants.py
@@ -1,4 +1,5 @@
 ble_chars = [
-    '0000ff02-0000-1000-8000-00805f9b34fb', # LithiumBatteryPCB unknown adapter
+    '0000ff01-0000-1000-8000-00805f9b34fb', # LithiumBatteryPCB adapter: read/notify
+    '0000ff02-0000-1000-8000-00805f9b34fb', # LithiumBatteryPCB adapter: write
     '0000ffe1-0000-1000-8000-00805f9b34fb', # TI CC245x (HM-10, HM-11)
 ]
