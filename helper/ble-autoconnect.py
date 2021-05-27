10b1adc472ad65c9d5741878a4529250ec30baa8
Jake <git@ja-ke.tech>
2021-05-28 00:12:58 +0200

    Autoconnect with config file #11 actually runs subprocess now

--- helper/ble-autoconnect.py
+++ helper/ble-autoconnect.py
@@ -5,27 +5,64 @@ then it should automatically start the specified tool (for example ble-serial) t
 
 import asyncio
 from bleak import BleakScanner
+from bleak.backends.device import BLEDevice
 
-from pprint import pprint
-from datetime import datetime
+import subprocess
 import signal
+import argparse, configparser
+
+async def run_tool(conf_section: dict):
+    await scanner.stop()
+
+    params = [conf_section['executable']] # binary name before args
+    for key, val in conf_section.items():
+        if key != 'executable': # add dashes to args
+            params.append(f'--{key}')
+            if val:
+                params.append(val)
+    print(params)
+
+    # Run target, passthrough stdout/stderr
+    proc = subprocess.run(params)
+    print('-> target exit code:', proc.returncode)
+
+    # Restart scanner
+    await scanner.start()
 
 
-def detection_callback(device, advertisement_data):
-    print(device.address, "RSSI:", device.rssi, advertisement_data)
+def detection_callback(device: BLEDevice, advertisement_data):
+    print(f'{device.address} = {device.name} (RSSI: {device.rssi})')
 
-async def main():
-    scanner = BleakScanner()
+    if device.address in config:
+        section = config[device.address]
+        loop.create_task(run_tool(section))
+    else:
+        print('-> Unknown device')
+
+def stop(signal=None, stackframe=None):
+    print(' signal received. Stopping scan!')
+    loop.create_task(scanner.stop())
+    loop.stop()
+
+async def start_scan():
     scanner.register_detection_callback(detection_callback)
     await scanner.start()
 
-    def stop_loop(signal, stackframe):
-        print('Stopping scan!')
-        loop.create_task(scanner.stop())
-        loop.stop()
-    signal.signal(signal.SIGINT, stop_loop)
+    signal.signal(signal.SIGINT, stop)
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, 
+            description='Service to automatically connect with devices that get available.')
+    parser.add_argument('-c', '--config', default='autoconnect.ini', required=False,
+            help='Path to a INI file with device configs')
+    args = parser.parse_args()
+
+    config = configparser.ConfigParser(allow_no_value=True)
+    with open(args.config, 'r') as f: # do it like this to detect non existand files
+        config.read_file(f)
+
+    scanner = BleakScanner()
+
     loop = asyncio.get_event_loop()
-    loop.create_task(main())
+    loop.create_task(start_scan())
     loop.run_forever()
