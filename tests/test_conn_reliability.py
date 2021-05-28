d316c5b389d36f9564bf57390b591605c922fc45
Jake <git@ja-ke.tech>
2021-05-29 01:10:41 +0200

    Add test for connection reliability based on deep scan, especially for `org.bluez.Error.Failed` #30

---
+++ tests/test_conn_reliability.py
@@ -0,0 +1,31 @@
+import sys, subprocess
+import time
+
+ble_mac = sys.argv[1]
+
+times = []
+results = []
+
+for i in range(100):
+    print(f'RUN {i+1} > ', end='')
+    t_start = time.perf_counter()
+    run = subprocess.run(['ble-scan', '-d', ble_mac, '-t', '30'], 
+        capture_output=True)
+    t = time.perf_counter()-t_start
+    times.append(t)
+
+    fail = b'ERROR' in run.stdout.upper()
+    results.append(not fail)
+
+    if fail:
+        print(f'FAILED after {t:.3f}')
+        print(run.stdout.decode())
+        break
+    else:
+        print(f'DONE in {t:.3f}s')
+    # time.sleep(3)
+
+print(f'success ratio {results.count(True)/len(results)*100:.1f} %')
+print(results)
+print(f'avg time {sum(times)/len(times)}s')
+print(times)
