e0bee83a871cc84a093cef43bc05536315ba2513
Jake <git@ja-ke.tech>
2022-02-14 23:47:11 +0100

    Put command args into groups

--- ble_serial/main.py
+++ ble_serial/main.py
@@ -25,30 +25,34 @@ class Main():
         parser.add_argument('-p', '--port', dest='port', required=False, default=DEFAULT_PORT,
             help=DEFAULT_PORT_MSG)
 
-        parser.add_argument('-d', '--dev', dest='device', required=False,
-            help='BLE device address to connect (hex format, can be separated by colons)')
-        parser.add_argument('-s', '--service-uuid', dest='service_uuid', required=False,
-            help='The service used for scanning of potential devices')
 
-        parser.add_argument('-t', '--timeout', dest='timeout', required=False, default=5.0, type=float, metavar='SEC',
+        con_group = parser.add_argument_group('connection parameters')
+        con_group.add_argument('-t', '--timeout', dest='timeout', required=False, default=5.0, type=float, metavar='SEC',
             help='BLE connect/discover timeout in seconds')
-        parser.add_argument('-a', '--address-type', dest='addr_type', required=False, choices=['public', 'random'], default='public',
-            help='BLE address type, either public or random')
-        parser.add_argument('-i', '--interface', dest='adapter', required=False, default='hci0',
+        con_group.add_argument('-i', '--interface', dest='adapter', required=False, default='hci0',
             help='BLE host adapter number to use')
-        parser.add_argument('-m', '--mtu', dest='mtu', required=False, default=20, type=int,
+        con_group.add_argument('-m', '--mtu', dest='mtu', required=False, default=20, type=int,
             help='Max. bluetooth packet data size in bytes used for sending')
 
-        parser.add_argument('-w', '--write-uuid', dest='write_uuid', required=False,
+        dev_group = parser.add_argument_group('device parameters')
+        dev_group.add_argument('-d', '--dev', dest='device', required=False,
+            help='BLE device address to connect (hex format, can be separated by colons)')
+        dev_group.add_argument('-a', '--address-type', dest='addr_type', required=False, choices=['public', 'random'], default='public',
+            help='BLE address type, either public or random')
+        dev_group.add_argument('-s', '--service-uuid', dest='service_uuid', required=False,
+            help='The service used for scanning of potential devices')
+            
+        dev_group.add_argument('-w', '--write-uuid', dest='write_uuid', required=False,
             help='The GATT characteristic to write the serial data, you might use "ble-scan -d" to find it out')
-        parser.add_argument('-r', '--read-uuid', dest='read_uuid', required=False,
+        dev_group.add_argument('-r', '--read-uuid', dest='read_uuid', required=False,
             help='The GATT characteristic to subscribe to notifications to read the serial data')
-        parser.add_argument('--permit', dest='mode', required=False, default='rw', choices=['ro', 'rw', 'wo'],
+        dev_group.add_argument('--permit', dest='mode', required=False, default='rw', choices=['ro', 'rw', 'wo'],
             help='Restrict transfer direction on bluetooth: read only (ro), read+write (rw), write only (wo)')
 
-        parser.add_argument('-l', '--log', dest='filename', required=False,
+        log_group = parser.add_argument_group('logging options')
+        log_group.add_argument('-l', '--log', dest='filename', required=False,
             help='Enable optional logging of all bluetooth traffic to file')
-        parser.add_argument('-b', '--binary', dest='binlog', required=False, action='store_true',
+        log_group.add_argument('-b', '--binary', dest='binlog', required=False, action='store_true',
             help='Log data as raw binary, disable transformation to hex. Works only in combination with -l')
 
         self.args = parser.parse_args()
