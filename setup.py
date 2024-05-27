a5202162b0b72cf9071960fdf59d78faf778b9db
Jake <git@ja-ke.tech>
2024-05-27 14:26:38 +0200

    Mark as python 3.12 compatible

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     python_requires='>=3.8',
     install_requires=REQUIRES,
