06f5e25c5c0663ca81b9e2b75f1e82e8e4dd7460
Jake <git@ja-ke.tech>
2024-12-07 02:22:24 +0100

    Run client args check only if in client mode

--- ble_serial/cli.py
+++ ble_serial/cli.py
@@ -65,6 +65,8 @@ def parse_args() -> Namespace:
 
 
 def client_checks(parser: ArgumentParser, args: Namespace):
+    if args.gap_role != 'client':
+        return # not applicable
     if not args.device and not args.service_uuid:
         parser.error('at least one of -d/--dev and -s/--service-uuid required')
 
